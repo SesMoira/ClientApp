c6b67db851e8d1580832d0dfd4fbd07c64cabf33
sesMoira <nokuhlegamede12345@gmail.com>
2021-11-17 19:59:33 +0200

    Add project files.

---
+++ ClientApp/Models/ClientStartup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClientApp.Models
+{
+    public partial class ClientStartup
+    {
+        public string Id { get; set; }
+        public int? Title { get; set; }
+        public string CapturedBy { get; set; }
+        public DateTime? CapturedDate { get; set; }
+        public string Geolocation { get; set; }
+        public string Description { get; set; }
+        public string ImageTag { get; set; }
+        public byte[] Image { get; set; }
+        public string AlbumName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
